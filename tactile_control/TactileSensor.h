dd1bd0258f783f506db660054a3567bbb27f6340
Niko Holmes <143398822+niko-holmes@users.noreply.github.com>
2024-01-25 08:52:33 +0000

    Added calibrate method

--- tactile_control/TactileSensor.h
+++ tactile_control/TactileSensor.h
@@ -7,15 +7,19 @@
 
 class TactileSensor{
   private:
-    byte mplxr_pin;
-    MLX90393 sensors[4] = {MLX90393(0b0001100), MLX90393(0b0001101), MLX90393(0b0001110), MLX90393(0b0001111)};
+    byte mplxrPin;
+    MLX90393 sensors[4] = {MLX90393(0b0001100), 
+                           MLX90393(0b0001101),
+                           MLX90393(0b0001110),
+                           MLX90393(0b0001111)};
 
     vector3 offset;
 
   public:
-    TactileSensor(byte mplxr_pin);
+    TactileSensor(byte mplxrPin);
     void init();
-    vector3_double readData();
+    void calibrate(int nSamples);
+    vector3Double readData();
 };
 
 #endif
