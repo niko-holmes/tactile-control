a6077621c88157cd86eee1d6e0b56ddd167fadf6
Niko Holmes <143398822+niko-holmes@users.noreply.github.com>
2024-01-25 09:02:39 +0000

    Switched to camel case var naming. Updated calculate method desc.

--- tactile_control/PID.cpp
+++ tactile_control/PID.cpp
@@ -10,25 +10,25 @@ PID::PID(int Kp, int Ki, int Kd){
 }
 
 void PID::init(){
-  start_time = millis();
-  last_error = 0;
-  cumulative_error = 0;
-  error_rate = 0;
+  startTime = millis();
+  lastError = 0;
+  cumulativeError = 0;
+  errorRate = 0;
 }
 
 int PID::calculate(double target, double output){
   /*Calculate plant input u(t). Calculate the error, error integral, and error derivative given the target and measured sensor output.
 
-  :target:  Target controller is trying to reach
-  :output:  Measured output from sensors
-  :return:  Control variable u(t) as an int
+  :param target:  Target controller is trying to reach
+  :param output:  Measured output from sensors
+  :returns:  Control variable u(t) as an int
   */
-  double elapsed_time = (millis() - start_time) / 1000.00;
+  double elapsedTime = (millis() - startTime) / 1000.00;
   double error = target - output;
 
-  cumulative_error += error * elapsed_time;
-  error_rate = (error - last_error) / elapsed_time;
-  last_error = error;
+  cumulativeError += error * elapsedTime;
+  errorRate = (error - lastError) / elapsedTime;
+  lastError = error;
 
-  return round((Kp * error) + (Ki * cumulative_error) + (Kd * error_rate));
+  return round((Kp * error) + (Ki * cumulativeError) + (Kd * errorRate));
 }
