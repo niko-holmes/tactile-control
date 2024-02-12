c6c078f234e89443b707d278a1760c09667b94b0
Nikolaus Holmes <143398822+niko-holmes@users.noreply.github.com>
2024-02-12 16:41:26 +0000

    Update PID.h

--- tactile_control/PID.h
+++ tactile_control/PID.h
@@ -1,20 +1,19 @@
 #ifndef PID_h
 #define PID_h
 
-#include "math.h"
-#include "Arduino.h"
+#include <Arduino.h>
 
 class PID{
   private:
-    int Kp, Ki, Kd;
-    unsigned long startTime;
-    double cumulativeError;
-    double errorRate;
-    double lastError;
+    int m_kp, m_ki, m_kd;
+    unsigned long m_start_time;
+    double m_cumulative_error;
+    double m_error_rate;
+    double m_last_error;
   
   public:
-    PID(int Kp, int Ki, int Kd);
-    int calculate(double target, double output);
+    PID(int t_kp, int t_ki, int t_kd);
+    int calculate(double t_target, double t_output);
 };
 
 #endif
