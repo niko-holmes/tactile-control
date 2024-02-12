ca26c9999393d84e88977ba95bae5f8bb5117246
Nikolaus Holmes <143398822+niko-holmes@users.noreply.github.com>
2024-02-12 16:42:41 +0000

    Update ServoBD.h

--- tactile_control/ServoBD.h
+++ tactile_control/ServoBD.h
@@ -1,16 +1,16 @@
 #ifndef ServoBD_h
 #define ServoBD_h
 
-#include "Arduino.h"
+#include <Arduino.h>
 
 class ServoBD{
   private:
-    byte pinF;         //Pin which sends forward signal to servo. Must be PWM pin (3,5,6,9,10,11 in Arduino UNO)
-    byte pinB;         //Pin which sends back signal to servo. Must be PWM pin (3,5,6,9,10,11 in Arduino UNO)
+    byte m_pin_f;         //Pin which sends forward signal to servo. Must be PWM pin (3,5,6,9,10,11 in Arduino UNO)
+    byte m_pin_b;         //Pin which sends back signal to servo. Must be PWM pin (3,5,6,9,10,11 in Arduino UNO)
 
   public:
-    ServoBD(byte pinF, byte pinB);
-    void pwm(int dutyCycle);
+    ServoBD(byte t_pin_f, byte t_pin_b);
+    void pwm(int t_duty_cycle);
 };
 
 #endif
