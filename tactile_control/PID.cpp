85b2d5127fc3dcfb1bd99b99d7e5b7f654803cd3
Nikolaus Holmes <143398822+niko-holmes@users.noreply.github.com>
2024-02-12 16:39:45 +0000

    Update PID.cpp

--- tactile_control/PID.cpp
+++ tactile_control/PID.cpp
@@ -1,29 +1,28 @@
-#include "Arduino.h"
 #include "PID.h"
 
-PID::PID(int Kp, int Ki, int Kd){
-  this->Kp = Kp;
-  this->Ki = Ki;
-  this->Kd = Kd;
-  startTime = millis();
-  lastError = 0;
-  cumulativeError = 0;
-  errorRate = 0;
+PID::PID(int t_kp, int t_ki, int t_kd){
+  m_kp = t_kp;
+  m_ki = t_ki;
+  m_kd = t_kd;
+  m_start_time = millis();
+  m_last_error = 0;
+  m_cumulative_error = 0;
+  m_error_rate = 0;
 }
 
-int PID::calculate(double target, double output){
+int PID::calculate(double t_target, double t_output){
   /*Calculate plant input u(t). Calculate the error, error integral, and error derivative given the target and measured sensor output.
 
-  :param target:  Target controller is trying to reach
-  :param output:  Measured output from sensors
-  :returns:  Control variable u(t) as an int
+  :param t_target:  Target controller is trying to reach
+  :param t_output:  Measured output from sensors
+  :return:  Control variable u(t) as an int
   */
-  double elapsedTime = (millis() - startTime) / 1000.00;
-  double error = target - output;
+  double elapsed_time = (millis() - m_start_time) / 1000.00;
+  double error = t_target - t_output;
 
-  cumulativeError += error * elapsedTime;
-  errorRate = (error - lastError) / elapsedTime;
-  lastError = error;
+  m_cumulative_error += error * elapsed_time;
+  m_error_rate = (error - m_last_error) / elapsed_time;
+  m_last_error = error;
 
-  return round((Kp * error) + (Ki * cumulativeError) + (Kd * errorRate));
+  return round((m_kp * error) + (m_ki * m_cumulative_error) + (m_kd * m_error_rate));
 }
